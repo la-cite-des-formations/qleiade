5461eb12b4a5c59188f4947782e59df5b743061c
PierreLagodiuk <pierre.lagodiuk@citeformations.com>
2025-11-05 10:57:15 +0100

    wealths admin menu filter updated

--- admin_panel/Orchid/Layouts/Parts/SearchLayout.php
+++ admin_panel/Orchid/Layouts/Parts/SearchLayout.php
@@ -3,16 +3,16 @@
 namespace Admin\Orchid\Layouts\Parts;
 
 use Models\Indicator;
+use Models\Unit;
+use Models\WealthType;
 use Models\QualityLabel;
 use Orchid\Screen\Field;
 use Orchid\Screen\Fields\Group;
 use Orchid\Screen\Fields\Relation;
-use Orchid\Screen\Layouts\Rows;
-use Models\Unit;
-use Models\WealthType;
-use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Fields\Select;
+use Orchid\Screen\Layouts\Rows;
+use Orchid\Screen\Actions\Link;
 
 class SearchLayout extends Rows
 {
@@ -41,6 +41,7 @@ class SearchLayout extends Rows
 
         // Options pour l'importance de la preuve
         $conformityOptions = [
+            '' => __('Select...'),
             'essentielle' => __('conformity_level_essential'),
             'complémentaire' => __('conformity_level_complementary'),
         ];
@@ -56,70 +57,50 @@ class SearchLayout extends Rows
                     ->type('search')
                     ->title(__('search_input'))
                     ->placeholder(__('Search...'))
-                    ->value(request('search.keyword'))
             ]),
 
             Group::make([
                 Select::make('search.units', __('units'))
                     ->options($unitOptions)
-                    ->displayAppend('full')
                     ->multiple()
                     ->title(__('units'))
-                    ->set('data-placeholder', __('Select...'))
-                    ->value(request('search.units') ? explode(',', request('search.units')[0]) : []),
+                    ->placeholder(__('Select...')),
 
-                Select::make('search.quality_label', __('quality_label'))
-                    // ->fromModel(QualityLabel::class, 'name', 'id')
-                    ->options($qualityLabelOptions)
+                Relation::make('search.quality_label', __('quality_label'))
+                    ->fromModel(QualityLabel::class, 'name', 'id') // <-- Définit le modèle et les champs
                     ->title(__('quality_label'))
-                    ->set('data-placeholder', __('Select...'))
-                    ->allowEmpty()
-                    ->value(request('search.quality_label')),
+                    ->placeholder(__('Select...')),
 
                 Relation::make('search.indicators', __('indicators'))
                     ->fromModel(Indicator::class, 'indicator.label', 'id')
                     ->multiple()
                     ->title(__('indicators'))
                     ->placeholder(__('Select...'))
-
-                    // AFFICHE l'accesseur 'full' (1.1 . Label)
                     ->displayAppend('full')
-
-                    // DÉCLENCHE l'AJAX quand 'quality_label' change
                     ->dependsOn('search.quality_label')
-
-                    // APPELLE notre nouveau scope pour filtrer ET trier
                     ->applyScope('byQualityLabelAndSort')
-
-                    // (Le tri des valeurs au chargement ne fonctionnera pas,
-                    // c'est notre compromis à cause du bug Orchid)
-                    ->value(request('search.indicators') ? explode(',', request('search.indicators')[0]) : []),
             ]),
 
             Group::make([
                 Relation::make('search.wealth_type', __('wealth_type'))
                     ->fromModel(WealthType::class, 'label', 'id')
-                    ->chunk(50)
                     ->title(__('wealth_type'))
-                    ->placeholder(__('Select...'))
-                    ->value(request('search.wealth_type')),
+                    ->placeholder(__('Select...')),
 
                 Select::make('search.conformity')
-                    ->title(__('conformity_level'))
                     ->options($conformityOptions)
-                    ->set('data-placeholder', __('Select...'))
-                    ->value(request('search.conformity')),
+                    ->title(__('conformity_level'))
+                    ->placeholder(__('Select...')),
 
                 Link::make('reinitialize', __('reinitialize'))
                     ->icon('reload')
-                    ->class('btn btn-outline-secondary')
                     ->route('platform.quality.wealths')
+                    ->class('btn btn-outline-secondary')
             ])->alignEnd(),
 
             // Champ caché pour le sort
             Input::make('sort')
-                ->type('hidden')
-                ->value(request('sort', '')),
+                ->type('hidden'),
         ];
     }
 }
